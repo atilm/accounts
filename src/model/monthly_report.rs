5423ffba48a12c30210cd65b94d3f18012b15546
Andreas Thielmann <thielmann.andreas@googlemail.com>
2024-09-16 21:27:56 +0200

    tests for monthly reports

--- src/model/monthly_report.rs
+++ src/model/monthly_report.rs
@@ -4,12 +4,19 @@ use chrono::Datelike;
 
 use super::AccountRecord;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 pub struct YearMonth {
     year: i32,
     month0: u32,
 }
 
+impl YearMonth {
+    pub fn new(year: i32, month0: u32) -> YearMonth {
+        YearMonth { year, month0 }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct MonthlyReport {
     month: YearMonth,
     records: Vec<AccountRecord>,
@@ -22,13 +29,13 @@ pub struct MonthlyReports {
 
 impl MonthlyReports {
     pub fn create(records: Vec<AccountRecord>) -> MonthlyReports {
-        let mut records_by_month: HashMap<YearMonth, Vec<AccountRecord>>  = HashMap::new();
+        let mut records_by_month: HashMap<YearMonth, Vec<AccountRecord>> = HashMap::new();
 
         // Group records by year-month combination
         for record in records {
             let year_month = YearMonth {
                 year: record.date.year(),
-                month0: record.date.month0()
+                month0: record.date.month0(),
             };
 
             if !records_by_month.contains_key(&year_month) {
@@ -42,22 +49,19 @@ impl MonthlyReports {
         // Convert into report
         let mut reports: Vec<MonthlyReport> = Vec::new();
         for (month, records) in records_by_month.into_iter() {
-            let report = MonthlyReport {
-                month,
-                records
-            };
+            let report = MonthlyReport { month, records };
             reports.push(report);
         }
 
-        MonthlyReports {
-            reports
-        }
+        MonthlyReports { reports }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{super::test_util::*, MonthlyReports};
+    use crate::model::monthly_report::YearMonth;
+
+    use super::{super::test_util::*, MonthlyReport, MonthlyReports};
 
     #[test]
     fn create_a_monthly_report() {
@@ -73,5 +77,31 @@ mod tests {
         let monthly_reports = MonthlyReports::create(given_records_from_various_months);
 
         assert!(monthly_reports.reports.len() == 3);
+
+        let expected_reports = vec![
+            MonthlyReport {
+                month: YearMonth::new(2023, 3),
+                records: vec![new_record(400.0, "6.4.2023")],
+            },
+            MonthlyReport {
+                month: YearMonth::new(2023, 2),
+                records: vec![
+                    new_record(-100.0, "5.3.2023"),
+                    new_record(200.0, "5.3.2023"),
+                    new_record(-300.0, "6.3.2023"),
+                ],
+            },
+            MonthlyReport {
+                month: YearMonth::new(2024, 2),
+                records: vec![
+                    new_record(-300.0, "5.3.2024"),
+                    new_record(400.0, "28.3.2024"),
+                ],
+            },
+        ];
+
+        assert!(monthly_reports.reports.contains(&expected_reports[0]));
+        assert!(monthly_reports.reports.contains(&expected_reports[1]));
+        assert!(monthly_reports.reports.contains(&expected_reports[2]));
     }
 }
